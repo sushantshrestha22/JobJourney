3f7c5ec7ff845256b030f5438ee3b7246464b14b
sushantshrestha22 <newasushant.ns16@gmail.com>
2024-08-04 16:17:47 +0545

    login

--- backend/frontend/frontend/src/component/Login.js
+++ backend/frontend/frontend/src/component/Login.js
@@ -1,10 +1,33 @@
-import React from 'react'
-import {Container} from 'react-bootstrap'
+import React from "react";
+import { Container, Form, Image, Button } from "react-bootstrap";
+import Imag from "../image/image1.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
 
 export default function Login() {
   return (
-    <Container>
-     <h1>hello</h1>
+    <Container fluid="md" class=" d-flex ">
+      <Image src={Imag} alt="" class="w-50"></Image>
+      <Container fluid class="w-50">
+        <h1 class="text-primary hello">Jobjourney</h1>
+        <h3>cheers !!! to the new start</h3>
+        <hr></hr>
+        <Form method="POST" action="login">
+          <Form.Group>
+            <Form.Label for="email">Email</Form.Label>
+            <Form.Control name="email" id="email"></Form.Control>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label for="password">Password</Form.Label>
+            <Form.Control name="password" id="password"></Form.Control>
+          </Form.Group>
+        </Form>
+        <Button type="submit">login</Button>
+        <Form.Text>
+          Don’t have account? <a href="#login">Register? </a>
+        </Form.Text>
+      </Container>
     </Container>
-  )
+  );
 }
