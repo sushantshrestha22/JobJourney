7a0705635816e027ca2271e33b4dcd7289a35193
sushantshrestha22 <newasushant.ns16@gmail.com>
2024-08-04 13:36:44 +0545

    frontend2

---
+++ frontend/frontend/src/component/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+
+export default function Login() {
+  return (
+    <Container>
+     <h1>hello</h1>
+    </Container>
+  )
+}
