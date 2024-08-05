0f9374cc386d6b6b28b4d1c064a4d107cba2d346
sushantshrestha22 <newasushant.ns16@gmail.com>
2024-08-05 14:23:15 +0545

    frontend

--- backend/frontend/frontend/src/component/Login.js
+++ backend/frontend/frontend/src/component/Login.js
@@ -12,25 +12,41 @@ export default function Login() {
         <Col>
           <Image src={Imag} alt="" class="w-50"></Image>
         </Col>
-        <Col>
-          <h1 class="text-primary hello">Jobjourney</h1>
-          <h3>cheers !!! to the new start</h3>
-          <hr></hr>
-          <Form method="POST" action="http://127.0.0.1:8000/home/">
-            <Form.Group>
-              <Form.Label for="email">Email</Form.Label>
-              <Form.Control name="email" id="email"></Form.Control>
-            </Form.Group>
+        <Col className="d-flex flex-column justify-content-center">
+          <Row>
+            <h1 class="text-primary hello">Jobjourney</h1>
+          </Row>
+          <Row>
+            <h3>cheers !!! to the new start</h3>
+          </Row>
+          <hr />
+          <Row>
+            <Form method="POST" action="http://127.0.0.1:8000/home/">
+              <Row>
+                <Form.Group>
+                  <Form.Label for="email">Email</Form.Label>
+                  <Form.Control name="email" id="email"></Form.Control>
+                </Form.Group>
+              </Row>
 
-            <Form.Group>
-              <Form.Label for="password">Password</Form.Label>
-              <Form.Control name="password" id="password"></Form.Control>
-            </Form.Group>
-            <Button type="submit">Login</Button>
-            <Form.Text>
-              Don’t have account? <Link to="/register">Register?</Link>
-            </Form.Text>
-          </Form>
+              <Row className="mt-2">
+                <Form.Group>
+                  <Form.Label for="password">Password</Form.Label>
+                  <Form.Control name="password" id="password"></Form.Control>
+                </Form.Group>
+              </Row>
+
+              <Row >
+                <Col>
+                  <Button type="submit" className="w-100 mt-3">Login</Button>
+                </Col>
+              </Row>
+              <Row className="mt-3">
+                  <Form.Text>Don’t have account? <Link to="/register">Register?</Link></Form.Text>
+                  
+              </Row>
+            </Form>
+          </Row>
         </Col>
       </Row>
 
